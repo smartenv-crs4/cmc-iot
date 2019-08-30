825f180601a829e36247198941b04559ea80e85a
Alessandro <a.romanino@gmail.com>
2019-08-30 17:39:29 +0200

    Update Test folders Name to this pattern "API-Test-NAME"

---
+++ test/API-Test-ApiActions/apiActions-api.js
@@ -0,0 +1,31 @@
+/*
+ ############################################################################
+ ############################### GPL III ####################################
+ ############################################################################
+ *                         Copyright 2019 CRS4                               *
+ *       This file is part of CRS4 Microservice Core - IoT (CMC-IoT).       *
+ *                                                                          *
+ *     CMC-IoT is free software: you can redistribute it and/or modify      *
+ *   it under the terms of the GNU General Public License as published by   *
+ *    the Free Software Foundation, either version 3 of the License, or     *
+ *                 (at your option) any later version.                      *
+ *                                                                          *
+ *    CMC-IoT is distributed in the hope that it will be useful,            *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *            GNU General Public License for more details.                  *
+ *                                                                          *
+ *     You should have received a copy of the GNU General Public License    *
+ *     along with CMC-IoT.  If not, see <http://www.gnu.org/licenses/>.     *
+ ############################################################################
+ */
+
+
+require('./Custom-ApiAction/CRUD-Tests');
+require('./Custom-ApiAction/searchFilterTests');
+require('./Custom-ApiAction/dataValidationTests');
+require('./From_Template/accessTokenSecurityTests');
+require('./From_Template/requestParserValidationTests');
+require('./From_Template/sqlInjectionSecurityTests');
+require('./From_Template/paginationTests');
+require('./From_Template/httpStatusCodeTests');
